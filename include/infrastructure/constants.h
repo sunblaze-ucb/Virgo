43cc00942e9f9df06d18c73617da8bfb727ac048
Tiancheng Xie <tianc.x@berkeley.edu>
2020-06-01 04:03:48 -0700

    Update constants.h

--- include/infrastructure/constants.h
+++ include/infrastructure/constants.h
@@ -3,7 +3,7 @@ const int max_fri_depth = 30;
 const int ldt_repeat_num = 33;
 //2^log_slice_number slices, each slice contains N/2^{log_slice_number+1} elements, each element is a pair of 
 //quad residue
-const int log_slice_number = 7;
+const int log_slice_number = 6;
 const int slice_number = 1 << log_slice_number;
 const int rs_code_rate = 5;
 const int max_bit_length = 30;
