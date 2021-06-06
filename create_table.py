10102e5cbc3183531a6d0e278daf5092246acd0a
Ubuntu <ubuntu@ip-172-31-21-139.us-west-2.compute.internal>
2021-06-06 01:33:16 +0000

    create_table

--- create_table.py
+++ create_table.py
@@ -4,12 +4,29 @@ import os
 p_time = []
 v_time = []
 proof_size = []
+N = 21
 
-
-for i in range(10, 11):
+for i in range(10, N):
     os.system('./bfc ' + str(i + 1) + ' ' + str(i))
-    os.system('./zk_proof fft.txt fft_meta.txt log' + str(i) + '.txt ' + str(i + 1) + ' ' + str(i) + '>> log' + str(i) + '.txt')
+    os.system('./zk_proof fft.txt fft_meta.txt log' + str(i) + '.txt ' + str(i + 1) + ' ' + str(i))
     f = open('log' + str(i) + '.txt')
 
     lines = f.readlines()
-    print(lines)
+    p, v, ps = lines[0].split(' ')
+    p_time.append(p)
+    v_time.append(v)
+    proof_size.append(int(str(ps)))
+
+
+lines = ['', '', '']
+
+
+for i in range(10, N):
+    lines[0] = lines[0] + ' ' + str(p_time[i - 10])
+    lines[1] = lines[1] + ' ' + str(v_time[i - 10])
+    lines[2] = lines[2] + ' ' + str(proof_size[i - 10])
+
+print(lines[0])
+print(lines[1])
+print(lines[2])
+
